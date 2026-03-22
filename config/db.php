5cbf69f43de57132eebd74c4d7a8c4a491b1df80
mileny velho <milenyvelho@outlook.com>
2026-03-22 09:56:32 -0300

    feat: projeto ecommerce completo com PHP e MySQL

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "ecommerce";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+  die("Erro de conexão: " . $conn->connect_error);
+}
+?>
